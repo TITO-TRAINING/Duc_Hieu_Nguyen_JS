b508cbadce7627d30712ce7f5de9b30a2959266e
Nguyen Duc Hieu <dhieu03@gmail.com>
2023-02-15 20:09:45 +0700

    add ex 3

--- p1_chapter2.js
+++ p1_chapter2.js
@@ -7,7 +7,7 @@ for (let f = 1; f<=7; f++) {
 }
 
 //FizzBuzz
-// console.log("---FizzBuzz---")
+console.log("---FizzBuzz---")
 
 for(var i = 1; i<=100; i++) {
     if(i % 3 == 0 && i % 5 == 0) {
@@ -22,3 +22,33 @@ for(var i = 1; i<=100; i++) {
     else console.log(i)
 }
  
+//ChessBoard
+console.log("---Chess Board---")
+
+var n = 8;
+var j = 1;
+var result = ''
+let evenChar = "#"
+let oddChar = " "
+while(j <= n*n ) {
+    if (j % 2 == 0) {
+        result += oddChar
+    }
+    else result += evenChar
+
+    if (j % n == 0) {
+        result += "\n"
+
+        if (evenChar == "#") {
+            evenChar = " "
+            oddChar = "#"
+        }
+        else {
+            evenChar = "#"
+            oddChar = " "
+        }
+    }   
+    j++
+}
+
+console.log(result)
